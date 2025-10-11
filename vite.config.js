6d3ea75b0aadb685212bbcf302284921deb06509
JOYSTON <116254639+Joystonm@users.noreply.github.com>
2025-10-11 10:02:48 +0530

    c

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     port: 3000,
     host: true,
